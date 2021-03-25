e28e3ebfacf281bd5ba629f69acb2bf793ff7abb
Adrien-Ray <forpub@accesdenied.net>
2021-03-25 16:01:20 +0100

    grid-boostrap

---
+++ grid.php
@@ -0,0 +1,71 @@
+<?php include "header.php" ; ?>
+    
+<div class="container-fluid"> <!-- contenant-->
+    <div class="row"> <!-- grilles de 12 unitées de large-->
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div><!-- md => ecran large, sm => reduit(mobile) -->
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+    </div>
+    <div class="row"> <!-- -->
+        <div class="col-md-4 col-sm-12 border-ad">Karina</div>
+        <div class="col-md-4 col-sm-12 border-ad">Sonia</div>
+        <div class="col-md-4 col-sm-12 border-ad">Romain</div>
+    </div>
+    <div class="row"> <!-- -->
+        <div class="col-md-2 col-sm-12 border-ad">Etienne</div>
+        <div class="col-md-8 col-sm-12 border-ad">Adam</div>
+        <div class="col-md-2 col-sm-12 border-ad">Adrien</div>
+    </div>
+</div>
+
+<div class="container"> <!-- contenant-->
+    <div class="row"> <!-- grilles de 12 unitées de large-->
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div><!-- md => ecran large, sm => reduit(mobile) -->
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+    </div>
+    <div class="row"> <!-- -->
+        <div class="col-md-4 col-sm-12 border-ad">Karina</div>
+        <div class="col-md-4 col-sm-12 border-ad">Sonia</div>
+        <div class="col-md-4 col-sm-12 border-ad">Romain</div>
+    </div>
+    <div class="row"> <!-- -->
+        <div class="col-md-4 col-sm-12 border-ad">Etienne</div>
+        <div class="col-md-8 col-sm-12 border-ad">
+        <div class="row"> <!-- grilles de 12 unitées de large-->
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div><!-- md => ecran large, sm => reduit(mobile) -->
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+        <div class="col-md-1 col-sm-12 border-ad">col-1</div>
+    </div>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ; ?>
