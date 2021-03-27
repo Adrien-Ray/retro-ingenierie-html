d4cbe80f45086992b824fe94735cf6b5c2fc1f4e
Adrien-Ray <forpub@accesdenied.net>
2021-03-27 20:45:45 +0100

    integration zone bleu

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php include "header.php" ; ?>
 
+<!-- DIV BLEU -->
+<div class="zoneblue">
+    <div class="container-fluid">   
+        <div class="row">
+            <div class="col-md-3 col-sm-1 border-ad"></div>
+            <div class="col-md-6 col-sm-10 border-ad">
+                <h1 class="h1-blue">Find the right partners to fuel your business growth</h1>
+                <p>Join a vibrant community of MSPs to forge long-lasting relationships with partners that help you create excellent customer experiences</p>
+            </div>
+            <div class="col-md-3 col-sm-1 border-ad"></div>
+        </div>
+    </div>
+</div>
+<div class="zoneblue2"></div>
+<!-- FIN DIV BLEU -->
+
 <?php include "footer.php" ; ?>
