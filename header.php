798082dea3669ab30f0d4c6e51c44a74b55a82c5
Adrien-Ray <forpub@accesdenied.net>
2021-03-27 13:58:20 +0100

    testdusamedi

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<main class="container-fluid contain-adr">
+<main class="container-fluid">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <img src="logo.png" alt="" class="logoimg"> <!-- LOGO ENTREPRISE -->
