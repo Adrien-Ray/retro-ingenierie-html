fbef998b6dd5301cdd99beecc643517fdd12aebc
Adrien-Ray <a.raymond@codeur.online>
2021-03-28 10:20:58 +0200

    version dimanche matin

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
             <div class="col-md-6 col-sm-10 border-ad">
                 <h1 class="h1-blue">Find the right partners to fuel your business growth</h1>
                 <p class="p-bluezone">Join a vibrant community of MSPs to forge long-lasting relationships with partners that help you create excellent customer experiences</p>
+                <p class="button-zonebleu"><a href="#" class="button-zonebleu">Register today</a></p>
             </div>
             <div class="col-md-3 col-sm-1 border-ad"></div>
         </div>
