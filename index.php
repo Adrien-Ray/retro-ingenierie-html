baee38c66e5b2bbf11f105b904918bd935b294a0
Adrien-Ray <a.raymond@codeur.online>
2021-03-29 14:26:33 +0200

    14h

--- index.php
+++ index.php
@@ -1,21 +1,137 @@
 <?php include "header.php" ; ?>
 
+
+
+
 <!-- DIV BLEU -->
+
+
 <div class="zoneblue">
     <!-- ici insertion img position absolute (+divbleu = position relative) et z-index -->
+    
+    
     <div class="container-fluid">   
         <div class="row">
-            <div class="col-md-3 col-sm-1 border-ad"></div>
+            <div class="col-md-3 col-sm-1 border-ad centerback"><img src="pictures/img-blue-left-opacity100.png" alt="" class="back-img blueleft"></div>
             <div class="col-md-6 col-sm-10 border-ad">
                 <h1 class="h1-blue">Find the right partners to fuel your business growth</h1>
                 <p class="p-bluezone">Join a vibrant community of MSPs to forge long-lasting relationships with partners that help you create excellent customer experiences</p>
                 <p class="button-zonebleu"><a href="#" class="button-zonebleu">Register today</a></p>
             </div>
-            <div class="col-md-3 col-sm-1 border-ad"></div>
+            <div class="col-md-3 col-sm-1 border-ad centerback"><img src="pictures/img-blue-right-opacity100.png" alt="" class="back-img blueright"></div>
         </div>
     </div>
 </div>
 <div class="zoneblue2"></div>
+
+
 <!-- FIN DIV BLEU -->
+
+
+<img src="pictures/back-titre-shell.png" alt="" class="back-fix-left">
+<img src="pictures/back-titre-shell.png" alt="" class="back-fix-right">
+
+
+<!-- CONTENTFLUID -->
+
+
+<div class="container">
+    <div class="row"> 
+        <div class="col-md-12 col-sm-12 content">
+            <h1>List. Sell. Grow</h1>
+            <div class="container-fluid">
+                <div class="row"> 
+                    <div class="col-md-4 col-sm-12 cardlistshellgrow">
+                        <img src="pictures/logo-shell-1.png" alt="" class="">
+                        <p>Be a part of the only All-In-One platform for IT services</p>
+                    </div>
+                    <div class="col-md-4 col-sm-12 cardlistshellgrow">
+                        <img src="pictures/logo-shell-2.png" alt="" class="">
+                        <p>Come closer to finding your next customer</p>
+                    </div>
+                    <div class="col-md-4 col-sm-12 cardlistshellgrow">
+                        <img src="pictures/logo-shell-3.png" alt="" class="">
+                        <p>Grow your business with a single click</p>
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="col-md-12 col-sm-12 contentline1">
+                        <H2 class="contentline">Your channel program  on steroids</H2>
+                        <p class="contentline">Tired of finding MSPs that can help you boost your channel sales? You’re in luck, because they’re all here.</p>
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="col-md-12 col-sm-12 contentline2">
+                        <H2 class="contentline"></H2>
+                        <p class="contentline"></p>
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="col-md-12 col-sm-12 contentline3">
+                        <H2 class="contentline"></H2>
+                        <p class="contentline"></p>
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="col-md-12 col-sm-12 contentline4">
+                        <H2 class="contentline"></H2>
+                        <p class="contentline"></p>
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="col-md-12 col-sm-12 contentline5">
+                        <H2 class="contentline"></H2>
+                        <p class="contentline"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<!-- FIN CONTENTFLUID -->
+
 
 <?php include "footer.php" ; ?>
